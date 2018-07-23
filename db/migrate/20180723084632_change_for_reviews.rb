3840faea0c3582ac84125bfc3c0b650a5c0c0987
Fanghaipeng <1219740008@qq.com>
2018-07-23 18:23:15 +0800

    修改了部分textarea

---
+++ db/migrate/20180723084632_change_for_reviews.rb
@@ -0,0 +1,5 @@
+class ChangeForReviews < ActiveRecord::Migration[5.2]
+  def change
+  	change_column :reviews, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_082307) do
+ActiveRecord::Schema.define(version: 2018_07_23_084632) do
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rpost_id"
